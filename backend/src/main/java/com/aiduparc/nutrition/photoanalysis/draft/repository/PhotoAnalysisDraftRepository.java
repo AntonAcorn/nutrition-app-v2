6703ebeefb5c26d573631a914d8dec10303cc46c
Anton <ShustikovAnimation@gmail.com>
2026-04-06 21:19:26 -0400

    Add Controller to fix a bug

--- backend/src/main/java/com/aiduparc/nutrition/photoanalysis/draft/repository/PhotoAnalysisDraftRepository.java
+++ backend/src/main/java/com/aiduparc/nutrition/photoanalysis/draft/repository/PhotoAnalysisDraftRepository.java
@@ -3,9 +3,12 @@ package com.aiduparc.nutrition.photoanalysis.draft.repository;
 import com.aiduparc.nutrition.photoanalysis.draft.model.PhotoAnalysisDraftEntity;
 import java.util.Optional;
 import java.util.UUID;
+
+import com.aiduparc.nutrition.photoanalysis.draft.model.PhotoAnalysisDraftStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotoAnalysisDraftRepository extends JpaRepository<PhotoAnalysisDraftEntity, UUID> {
     Optional<PhotoAnalysisDraftEntity> findById(UUID id);
+    Optional<PhotoAnalysisDraftEntity> findTopByStatusOrderByCreatedAtDesc(PhotoAnalysisDraftStatus status);
 }
 
