57a5bd0858819eff7651ee82893f2a3ac700b7bf
Anton <ShustikovAnimation@gmail.com>
2026-05-05 22:11:17 -0400

    iOS: load app from rumblyeats.org to fix cross-origin session cookies

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.aiduparc.rumblyeats',
   appName: 'Rumbly Eats',
   webDir: 'dist',
+  server: {
+    url: 'https://rumblyeats.org',
+    cleartext: false,
+  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 0,
